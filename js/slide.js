d19716b1f5ebc240340c14c718e988b586a3a5bc
Aliyu Kamilu <aliyukamilu@steamledge.com>
2021-05-03 05:44:53 +0100

    First Upload

---
+++ js/slide.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(".registration-form fieldset:first-child").fadeIn("fast");
+  
+    // next step
+    $(".registration-form .btn-next").on("click", function () {
+      var parent_fieldset = $(this).parents("fieldset");
+      var next_step = true;
+  
+      if (next_step) {
+        parent_fieldset.fadeOut(400, function () {
+          $(this).next().fadeIn();
+        });
+      }
+    });
+    
+    // previous step
+    $(".registration-form .btn-previous").on("click", function () {
+      $(this)
+        .parents("fieldset")
+        .fadeOut(400, function () {
+          $(this).prev().fadeIn();
+        });
+    });
+  });
+  
